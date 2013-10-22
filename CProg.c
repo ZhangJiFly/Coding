c6b3a39845f6fac87c42abe465d49c5946ddcd08
Joshua Marks <jmarks.joshua@googlemail.com>
2013-10-22 14:47:42 +0100

    sorting emails attempt 2

---
+++ CProg.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(){
+FILE *fp;
+char outputLine[100];
+char name[20];
+char emailAddress[50];
+char ofp[100];
+
+fp = fopen("/Users/Crippled.Josh/Desktop/RecEmails.csv","r");
+if(fp == NULL){
+      perror("Error opening file");
+      return(-1);
+}
+
+while (fscanf(fp, "%s <%s", name, emailAddress) != EOF) {
+  sprintf(outputLine, "%s %s\n", name, emailAddress);
+}
+
+return -1;
+}
