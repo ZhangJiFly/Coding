9087824937839aa595975e9150c0ea302065ed35
Joshua Marks <jmarks.joshua@googlemail.com>
2013-12-01 18:47:23 +0000

    Ai redo after mac spill

--- AI4/AILab2.py
+++ AI4/AILab2.py
@@ -1,23 +1,24 @@
-from numpy import *
-from mathsFunctions import *
 import mathsFunctions as mf
-#import numpy as np
 import matplotlib.pyplot as plt
+from math import *
+import random as random
 
-Rect = []
-ELogAv = []
-MLogAv = []
-ZAv = []
-
-for i in range (0,240):
-    Rect.append(1)
+def readFile(fileName):
+    f = open(fileName)
+    array = f.readlines()
+    array = [int(numStr) for numStr in array]
+    return array
 
 def values(name, i):
-    global ELogAv, MLogAv, ZAv, Rect
     number = str(i)
     if (i < 10):
         number = "0" + str(i)
-    DataIn = loadtxt("/Users/Crippled.Josh/Coding/AI4/silence_speech/" + name + "_" + number + ".dat")
+
+    Rect = []
+    for i in range (0,240):
+        Rect.append(1)
+        
+    DataIn = readFile("./silence_speech/" + name + "_" + number + ".dat")
 
     sampleRate = len(DataIn)/0.3
     Datafft = []
@@ -38,24 +39,107 @@ def values(name, i):
     Magn = mf.convolve(Datapos, Rect)
     ZCR = mf.convolve(Datasign, Rect)
     
-    ELogAv.append(mf.logBase(mf.mean(Energy), exp(1)))
-    MLogAv.append(mf.logBase(mf.mean(Magn), exp(1)))
-    ZAv.append(mf.mean(ZCR))
-    
+    E = mf.logBase(mf.mean(Energy), exp(1))
+    M = mf.logBase(mf.mean(Magn), exp(1))
+    Z = mf.mean(ZCR)
+    return E, M, Z
+
+def setEMZ(array):
+    E = []
+    M = []
+    Z = []
+    for i in range (0,len(array)):
+        E.append(array[i][0])
+        M.append(array[i][1])
+        Z.append(array[i][2])
+    return E, M, Z
+
+def postSil(sample):
+    global ESil, MSil, ZSil
+    return 0.5 * mf.probDens(ESil,sample[0]) * mf.probDens(MSil, sample[1]) * mf.probDens(ZSil, sample[2])
+
+def postSpe(sample):
+    global ESpe, MSpe, ZSpe
+    return  0.5 * mf.probDens(ESpe,sample[0]) * mf.probDens(MSpe, sample[1]) * mf.probDens(ZSpe, sample[2])
+
+def genKSet(Sil, Spe):
+    KSets = []
+    random.shuffle(Sil)
+    random.shuffle(Spe)
+    for i in range (0,10):
+        KSets.append([Sil[i*5:(i+1)*5], Spe[i*5:(i+1)*5]])
+    return KSets
+
+def reform(Kset):
+    Sil = []
+    Spe = []
+    for i in range (0, len(Kset)):
+        Sil.append(KSet[i][0])
+        Spe.append(KSet[i][1])
+    Sil = dechunk(Sil)
+    Spe = dechunk(Spe)
+    return Sil, Spe
+
+def dechunk(array):
+    dechunked = []
+    for i in range (0,len(array)):      
+       for j in range (0, len(array[i])):
+            dechunked.append(array[i][j])
+    return dechunked
+
+Sil = []
+Spe = []
 
 for i in range (1,51):
-    values("silence", i)
+    Sil.append(values("silence", i))
 for i in range (1,51):
-    values("speech", i)
-    
+    Spe.append(values("speech", i))
+
+
+KSet = genKSet(Sil, Spe)
+correctCount = 0
+errorCount = 0
+for i in range(0,10):
+    trainingSet = []
+    for j in range (0,10):
+        if (j != i):
+            trainingSet.append(KSet[j])
+    testSet = [KSet[i]]
+    trainSil, trainSpe = reform(trainingSet)
+    print "trainingSet: " + str(len(trainingSet))
+    print "trainSil: " + str(len(trainSil))
+    ESil, MSil, ZSil = setEMZ(trainSil)
+    ESpe, MSpe, ZSpe = setEMZ(trainSpe)
+    testSil, testSpe = reform(testSet)
+    for i in range (0, len(testSil)):
+        if (postSil(testSil[i]) > postSpe(testSil[i])):
+            correctCount += 1
+        else:
+            errorCount += 1
+    for i in range (0, len(testSpe)):
+        if (postSpe(testSpe[i]) > postSil(testSpe[i])):
+            correctCount += 1
+        else:
+            errorCount += 1
+
+print mf.variance([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17])
+
+ESil, MSil, ZSil = setEMZ(Sil)
+ESpe, MSpe, ZSpe = setEMZ(Spe)
+
+print correctCount
+print errorCount
+
 plt.figure(1)
 plt.subplot(311)
-plt.scatter(ELogAv, MLogAv)
+plt.scatter(ESil, MSil, color = 'red')
+plt.scatter(ESpe, MSpe, color = 'blue')
 plt.subplot(312)
-plt.scatter(ELogAv, ZAv)
+plt.scatter(ESil, ZSil, color = 'red')
+plt.scatter(ESpe, ZSpe, color = 'blue')
 plt.subplot(313)
-plt.scatter(MLogAv, ZAv)
-plt.legend(['x = ELogAv y = MLogAv', 'x = ELogAv y = ZAv', 'x = MLogAv y = ZAv'], loc='upper left')
+plt.scatter(MSil, ZSil, color = 'red')
+plt.scatter(MSpe, ZSpe, color = 'blue')
 
 plt.show()
     
--- AI4/mathsFunctions.py
+++ AI4/mathsFunctions.py
@@ -1,14 +1,16 @@
 from math import *
+import numpy as np
 
 def convolve(array1, window):
     array2 = []
 
-    for i in range (0, len(array1)):
-        low = max(i-len(window)/2, 0)
-        high = min(i+len(window)/2, len(array1))
-        total = sum(array1[low:high])
-        array2.append(total)
-    return array2
+##    for i in range (0, len(array1)):
+##        low = max(i-len(window)/2, 0)
+##        high = min(i+len(window)/2, len(array1))
+##        total = sum(array1[low:high])
+##        array2.append(total)
+##    return array2
+    return np.convolve(array1, window, mode='same')
     
 def divide(array1, x):
     array2 = []
@@ -44,13 +46,11 @@ def square(array1):
     return array2
 
 def mean(array):
-    mean = 0
-
+    total = 0.0
     for i in range (0, len(array)):
-        mean += array[i]
-    mean = mean/len(array)
+        total += array[i]
+    mean = total/len(array)
     return mean
-    #return np.mean(array)
 
 def logBase(x, base):
     return log(x,base)
@@ -77,4 +77,17 @@ def linSpace(array1):
         array2.append(i*space)
     return array2
     
-    
+def variance(array):
+    array2 = []
+    m = mean(array)
+    for i in range(0,len(array)):
+        diff = (array[i]-m)**2
+        array2.append(diff)
+    return mean(array2)
+
+def probDens(array, quality):
+    m = mean(array)
+    var = variance(array)
+    first = (sqrt(2*pi*var))
+    second = exp(-(quality-m)**2/(2*var))
+    return second/first
