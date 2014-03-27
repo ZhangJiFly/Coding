8d86dabe59e89f91666c43a2ccb603c54b18fa10
Joshua Marks <jmarks.joshua@googlemail.com>
2014-03-27 11:08:37 +0000

    Fixed some bugs made the server computer and SQL version non specifc

--- Dissertation/EER-test.sql
+++ Dissertation/EER-test.sql
@@ -2,39 +2,39 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-DROP SCHEMA IF EXISTS `mydb` ;
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
-USE `mydb` ;
+DROP SCHEMA IF EXISTS `kylevdm_mydb` ;
+CREATE SCHEMA IF NOT EXISTS `kylevdm_mydb` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE `kylevdm_mydb` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`College`
+-- Table `kylevdm_mydb`.`College`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`College` (
+CREATE TABLE IF NOT EXISTS `kylevdm_mydb`.`College` (
   `Name` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`Name`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`School`
+-- Table `kylevdm_mydb`.`School`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`School` (
+CREATE TABLE IF NOT EXISTS `kylevdm_mydb`.`School` (
   `Name` VARCHAR(45) NOT NULL,
   `College` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`Name`),
   INDEX `fk_School_College1_idx` (`College` ASC),
   CONSTRAINT `fk_School_College1`
     FOREIGN KEY (`College`)
-    REFERENCES `mydb`.`College` (`Name`)
+    REFERENCES `kylevdm_mydb`.`College` (`Name`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Staff`
+-- Table `kylevdm_mydb`.`Staff`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Staff` (
+CREATE TABLE IF NOT EXISTS `kylevdm_mydb`.`Staff` (
   `StaffId` VARCHAR(10) NOT NULL,
   `Forename` VARCHAR(45) NOT NULL,
   `Surname` VARCHAR(45) NOT NULL,
@@ -47,30 +47,30 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Staff` (
   INDEX `fk_Staff_School1_idx` (`School` ASC),
   CONSTRAINT `fk_Staff_School1`
     FOREIGN KEY (`School`)
-    REFERENCES `mydb`.`School` (`Name`)
+    REFERENCES `kylevdm_mydb`.`School` (`Name`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Advisor`
+-- Table `kylevdm_mydb`.`Advisor`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Advisor` (
+CREATE TABLE IF NOT EXISTS `kylevdm_mydb`.`Advisor` (
   `StaffId` VARCHAR(10) NOT NULL,
   PRIMARY KEY (`StaffId`),
   CONSTRAINT `fk_Advisor_Staff1`
     FOREIGN KEY (`StaffId`)
-    REFERENCES `mydb`.`Staff` (`StaffId`)
+    REFERENCES `kylevdm_mydb`.`Staff` (`StaffId`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Degree`
+-- Table `kylevdm_mydb`.`Degree`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Degree` (
+CREATE TABLE IF NOT EXISTS `kylevdm_mydb`.`Degree` (
   `Name` VARCHAR(45) NOT NULL,
   `School` VARCHAR(45) NOT NULL,
   `Years` INT NULL,
@@ -79,16 +79,16 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Degree` (
   INDEX `fk_Degree_School1_idx` (`School` ASC),
   CONSTRAINT `fk_Degree_School1`
     FOREIGN KEY (`School`)
-    REFERENCES `mydb`.`School` (`Name`)
+    REFERENCES `kylevdm_mydb`.`School` (`Name`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Student`
+-- Table `kylevdm_mydb`.`Student`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Student` (
+CREATE TABLE IF NOT EXISTS `kylevdm_mydb`.`Student` (
   `Matric` VARCHAR(8) NOT NULL,
   `Email` VARCHAR(255) NULL,
   `Password` VARCHAR(500) NOT NULL,
@@ -104,49 +104,49 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Student` (
   INDEX `fk_Student_Degree1_idx` (`Degree` ASC),
   CONSTRAINT `fk_Student_Advisor1`
     FOREIGN KEY (`StaffId`)
-    REFERENCES `mydb`.`Advisor` (`StaffId`)
+    REFERENCES `kylevdm_mydb`.`Advisor` (`StaffId`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Student_Degree1`
     FOREIGN KEY (`Degree`)
-    REFERENCES `mydb`.`Degree` (`Name`)
+    REFERENCES `kylevdm_mydb`.`Degree` (`Name`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`DeptHead`
+-- Table `kylevdm_mydb`.`DeptHead`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`DeptHead` (
+CREATE TABLE IF NOT EXISTS `kylevdm_mydb`.`DeptHead` (
   `StaffId` VARCHAR(10) NOT NULL,
   `Year` VARCHAR(45) NULL,
   PRIMARY KEY (`StaffId`),
   CONSTRAINT `fk_Dept Head_Staff1`
     FOREIGN KEY (`StaffId`)
-    REFERENCES `mydb`.`Staff` (`StaffId`)
+    REFERENCES `kylevdm_mydb`.`Staff` (`StaffId`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`TeachingCommittee`
+-- Table `kylevdm_mydb`.`TeachingCommittee`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`TeachingCommittee` (
+CREATE TABLE IF NOT EXISTS `kylevdm_mydb`.`TeachingCommittee` (
   `StaffId` VARCHAR(10) NOT NULL,
   PRIMARY KEY (`StaffId`),
   CONSTRAINT `fk_Teaching Committee_Staff1`
     FOREIGN KEY (`StaffId`)
-    REFERENCES `mydb`.`Staff` (`StaffId`)
+    REFERENCES `kylevdm_mydb`.`Staff` (`StaffId`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Course`
+-- Table `kylevdm_mydb`.`Course`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Course` (
+CREATE TABLE IF NOT EXISTS `kylevdm_mydb`.`Course` (
   `CourseId` VARCHAR(15) NOT NULL,
   `Name` VARCHAR(100) NULL,
   `Description` VARCHAR(1000) NULL,
@@ -161,16 +161,16 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Course` (
   INDEX `fk_Course_School1_idx` (`School` ASC),
   CONSTRAINT `fk_Course_School1`
     FOREIGN KEY (`School`)
-    REFERENCES `mydb`.`School` (`Name`)
+    REFERENCES `kylevdm_mydb`.`School` (`Name`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`StudentHasCourse`
+-- Table `kylevdm_mydb`.`StudentHasCourse`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`StudentHasCourse` (
+CREATE TABLE IF NOT EXISTS `kylevdm_mydb`.`StudentHasCourse` (
   `StudentHasCourseId` VARCHAR(45) NOT NULL,
   `Grade` ENUM('A1','A2','A3','A4','A5','B1','B2','B3','C1','C2','C3','D1','D2','D3','E1','E2','E3','F1','F2','F3','G1','G2','H','') NULL,
   `Status` ENUM('Completed','','Uncompleted','Failed') NULL,
@@ -181,20 +181,20 @@ CREATE TABLE IF NOT EXISTS `mydb`.`StudentHasCourse` (
   INDEX `fk_StudentHasCourse_Course1_idx` (`CourseId` ASC),
   CONSTRAINT `fk_StudentHasCourse_Student1`
     FOREIGN KEY (`Matric`)
-    REFERENCES `mydb`.`Student` (`Matric`)
+    REFERENCES `kylevdm_mydb`.`Student` (`Matric`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_StudentHasCourse_Course1`
     FOREIGN KEY (`CourseId`)
-    REFERENCES `mydb`.`Course` (`CourseId`)
+    REFERENCES `kylevdm_mydb`.`Course` (`CourseId`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`CourseList`
+-- Table `kylevdm_mydb`.`CourseList`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`CourseList` (
+CREATE TABLE IF NOT EXISTS `kylevdm_mydb`.`CourseList` (
   `CourseListId` VARCHAR(45) NOT NULL,
   `School` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`CourseListId`))
@@ -202,9 +202,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Groups`
+-- Table `kylevdm_mydb`.`Groups`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Groups` (
+CREATE TABLE IF NOT EXISTS `kylevdm_mydb`.`Groups` (
   `GroupId` INT NOT NULL,
   `Minimum` INT NULL,
   `CourseListId` VARCHAR(45) NOT NULL,
@@ -213,16 +213,16 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Groups` (
   INDEX `fk_Group_CourseList1_idx` (`CourseListId` ASC),
   CONSTRAINT `fk_Group_CourseList1`
     FOREIGN KEY (`CourseListId`)
-    REFERENCES `mydb`.`CourseList` (`CourseListId`)
+    REFERENCES `kylevdm_mydb`.`CourseList` (`CourseListId`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`DegreeHasCourseList`
+-- Table `kylevdm_mydb`.`DegreeHasCourseList`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`DegreeHasCourseList` (
+CREATE TABLE IF NOT EXISTS `kylevdm_mydb`.`DegreeHasCourseList` (
   `CourseListId` VARCHAR(45) NULL,
   `Degree` VARCHAR(45) NOT NULL,
   `School` VARCHAR(45) NOT NULL,
@@ -232,21 +232,21 @@ CREATE TABLE IF NOT EXISTS `mydb`.`DegreeHasCourseList` (
   INDEX `fk_DegreeHasCourseList_Degree1_idx` (`Degree` ASC, `School` ASC),
   CONSTRAINT `fk_DegreeHasCourseList_CourseList1`
     FOREIGN KEY (`CourseListId`)
-    REFERENCES `mydb`.`CourseList` (`CourseListId`)
+    REFERENCES `kylevdm_mydb`.`CourseList` (`CourseListId`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_DegreeHasCourseList_Degree1`
     FOREIGN KEY (`Degree` , `School`)
-    REFERENCES `mydb`.`Degree` (`Name` , `School`)
+    REFERENCES `kylevdm_mydb`.`Degree` (`Name` , `School`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`GroupHasCourse`
+-- Table `kylevdm_mydb`.`GroupHasCourse`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`GroupHasCourse` (
+CREATE TABLE IF NOT EXISTS `kylevdm_mydb`.`GroupHasCourse` (
   `GroupId` INT NOT NULL,
   `CourseListId` VARCHAR(45) NOT NULL,
   `CourseId` VARCHAR(15) NOT NULL,
@@ -255,12 +255,12 @@ CREATE TABLE IF NOT EXISTS `mydb`.`GroupHasCourse` (
   INDEX `fk_GroupHasCourse_Course1_idx` (`CourseId` ASC),
   CONSTRAINT `fk_Course_has_Group_Group1`
     FOREIGN KEY (`GroupId` , `CourseListId`)
-    REFERENCES `mydb`.`Groups` (`GroupId` , `CourseListId`)
+    REFERENCES `kylevdm_mydb`.`Groups` (`GroupId` , `CourseListId`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_GroupHasCourse_Course1`
     FOREIGN KEY (`CourseId`)
-    REFERENCES `mydb`.`Course` (`CourseId`)
+    REFERENCES `kylevdm_mydb`.`Course` (`CourseId`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
--- Dissertation/code/node/app.js
+++ Dissertation/code/node/app.js
@@ -1,5 +1,7 @@
 var mysql = require('mysql'),
 
+
+
 connection = mysql.createConnection({
     host     : '127.0.0.1',
     user     : 'root',
@@ -12,18 +14,10 @@ server = require('routes')(auth, query),
 io = require('socket.io').listen(server);
 
 
-var createYear = function(num){
-  return "<div class='row'><div class='panel panel-primary'><div class='panel-heading'>"+ num + "Year Course List</div><div class='panel-body'><div class='w2ui-label'> List:</div><div class='w2ui-field'><input type='list' style='width: 200px; box-sizing: border-box; padding-right: 14px;' placeholder=' class='w2field'/><div style='color: rgb(0, 0, 0); font-size: 14px; padding: 3px; margin-top: 1px; margin-bottom: 1px; margin-left: -16px'; class='w2ui-field-helper'></div><span class='legend'></span><div style='height: 30px;'></div></div></div></div></div>";
-}
-
-var createYears = function(){
-  for(var i= 0; i<4;i++){
-    var string = createYear(i);
-    $('#years').append(string);
-  }
-}
-
+//connection.connect();
 query.updateStart(pass);
+
+
 
 io.sockets.on('connection', function (socket) { 
   socket.on('courseList', function (data) {
